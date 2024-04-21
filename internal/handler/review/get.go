c014d66a76a6620438080937e05559f33ccc41f4
go-jedi <golangjedi@gmail.com>
2024-04-21 16:20:53 +0300

    add reviews, projects get if nil return default slice in result

--- internal/handler/review/get.go
+++ internal/handler/review/get.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gofiber/fiber/v3"
 
+	"github.com/go-jedi/portfolio/internal/model/review"
 	"github.com/go-jedi/portfolio/pkg/logger"
 )
 
@@ -45,6 +46,13 @@ func (h *Handler) Get(c fiber.Ctx) error {
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": err.Error(),
+		})
+	}
+
+	if len(result) == 0 {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "успешное получение отзывов",
+			"result":  []review.Review{},
 		})
 	}
 
